d6bf4af047ac22b3897921f6cf55cbf010cd7804
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-08-17 10:11:44 +0300

    window.navigator mocking

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,15 @@
 import sum from './sum';
 
+afterEach(() => {
+  Object.defineProperty(window.navigator, 'language', {value: 'en', configurable: true});
+});
+
+test('window.navigator.language', () => {
+  Object.defineProperty(window.navigator, 'language', {value: 'ru-RU', configurable: true});
+  expect(sum(1, 2)).toBe(3);
+});
+
 test('1+2=3', () => {
+  const sum = jest.fn((a, b) => {return a + b;});
   expect(sum(1, 2)).toBe(3);
 });
