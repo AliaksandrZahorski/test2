6f2f70c81f1fc51e72e48b7a3498bebb9b9c3b5c
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2017-10-24 17:45:16 +0300

    example of table design

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
+
+
 If you want to try application:
+
+
 
 git clone https://github.com/AliaksandrZahorski/test2.git
 
+
+
 cd test2
+
+
 
 npm install
 
+
+
 npm run start
 
+
+
 open http://localhost:3000
+
+http://www.material-ui.com/#/components/table
+
+https://uui.epam.com/ui-elements/tables/basic-tables
--- src/TableBody.jsx
+++ src/TableBody.jsx
@@ -5,7 +5,11 @@ render() {
   const listBody = this.props.data.map((row, index) => {
      return (
        <div key={index} className={this.props.table_body}>
-          {this.props.header.map((h) => <div key={h.name} className={this.props.table_element}> {row[h.name]} </div>)}
+          {this.props.header.map((h) =>
+            <div key={h.name} className={this.props.table_element}>
+              {row[h.name]}
+            </div>)
+          }
        </div>
      );
   });
